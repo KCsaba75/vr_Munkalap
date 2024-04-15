45e1f6c7adb4466f0adce17a0393e277d52323ab
KCsaba75 <app.fejleszt@gmail.com>
2024-04-15 14:12:52 +0200

    csaba1

--- config/urls.py
+++ config/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('api/munkalapUj/', views. UjMunkalapCreateView.as_view(), name='uj_munkalap_json'), 
     path('api/gepjarmuOsszes/', views. OsszesGepjarmuJsonView.as_view(), name='osszes_gepjarmuvek_json'), 
 
+
 ]
